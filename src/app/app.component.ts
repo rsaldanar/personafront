d4dd9f208e2ca741cf52736b68a3f79f2831fce5
Rafael Saldaña R <r.saldana@dominicanadeseguros.com>
2023-05-24 22:24:54 -0400

    S

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,39 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
+import { EstadosService } from './servises/estados/estados.service';
+import { PaisesService } from './servises/paises/paises.service';
+import { PersonaService } from './servises/persona/persona.service';
+
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'personafront';
+export class AppComponent implements  OnInit{
+
+  personaForm!: FormGroup;
+  constructor(
+  public fb: FormBuilder,
+  public estadosService: EstadosService,
+  public paisesService: PaisesService,
+  public personaService: PersonaService
+  ){
+
+  }
+
+  ngOnInit(): void {
+//   this.personaFrorm = this.fb.group({
+//     nombre : ['', Validators.required],
+//     apellido : ['', Validators.required],
+//     edad : ['', Validators.required],
+//     pais : ['', Validators.required],
+//     estado : ['', Validators.required],
+//    });
+  }
+
+  guardar(): void{
+//
+  }
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,13 +4,19 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    ReactiveFormsModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
